57b6cff5f4b0e0e75cf48b7eddf697dfce5fb3a6
Alex1s <Alex1s@users.noreply.github.com>
2024-01-16 21:18:36 +0100

    copy variables over because they are static ...

--- components/esp32_ble_hue_light/esp32_ble_hue_light_component.cpp
+++ components/esp32_ble_hue_light/esp32_ble_hue_light_component.cpp
@@ -10,6 +10,11 @@
 namespace esphome {
 namespace esp32_ble_hue_light {
 
+static const uint16_t DEVICE_INFORMATION_SERVICE_UUID = 0x180A;
+static const uint16_t MODEL_UUID = 0x2A24;
+static const uint16_t VERSION_UUID = 0x2A26;
+static const uint16_t MANUFACTURER_UUID = 0x2A29;
+
 static const char *const TAG = "esp32_ble_hue_light.component";
 static const char *const ESPHOME_MY_LINK = "https://my.home-assistant.io/redirect/config_flow_start?domain=esphome";
 
@@ -74,8 +79,8 @@ void ESP32ImprovComponent::loop() {
   }
   if (this->service_ == nullptr) {
     // update Device Information
-    BLEService *device_information_service = global_ble_server->get_service(ESPBTUUID::from_uint16(esp32_ble_server::DEVICE_INFORMATION_SERVICE_UUID));
-    BLECharacteristic *manufacturer_name_string_characteristic = device_information_service->get_characteristic(esp32_ble_server::MANUFACTURER_UUID);
+    BLEService *device_information_service = global_ble_server->get_service(ESPBTUUID::from_uint16(DEVICE_INFORMATION_SERVICE_UUID));
+    BLECharacteristic *manufacturer_name_string_characteristic = device_information_service->get_characteristic(MANUFACTURER_UUID);
     manufacturer_name_string_characteristic->set_value("Signify"); // does this now actually auto update?
     global_ble_server->set_manufacturer("Signify"); // just for consistency, does not actually do anything ...
     global_ble_server->set_manufacturer_data(std::vector<uint8_t>{0x02, 0x10, 0xFF, 0xFF, 0x05});
