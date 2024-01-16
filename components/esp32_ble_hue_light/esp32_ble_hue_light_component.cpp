61fb0e1a17fc49dc661f6a8633e71c4a2c268f51
Alex1s <Alex1s@users.noreply.github.com>
2024-01-16 20:06:59 +0100

    rename files

--- components/esp32_ble_hue_light/esp32_ble_hue_light_component.cpp
+++ components/esp32_ble_hue_light/esp32_ble_hue_light_component.cpp
@@ -1,4 +1,4 @@
-#include "esp32_improv_component.h"
+#include "esp32_ble_hue_light_component.h"
 
 #include "esphome/components/esp32_ble/ble.h"
 #include "esphome/components/esp32_ble_server/ble_2902.h"
